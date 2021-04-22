450ff19636f2e0d13f5accd7601d46f02fec6add
V-mari <violettrujillo565@gmail.com>
2021-04-22 13:23:27 -0400

    mi primer Ruth

---
+++ app/src/main/java/com/example/holamundo/hola_mundo.java
@@ -0,0 +1,14 @@
+package com.example.holamundo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hola_mundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo);
+    }
+}
